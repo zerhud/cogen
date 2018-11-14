7600c074433f2cc18d772bcf95403fbcc114ad9d
fleonis <fleonis@noemail.net>
2018-11-14 22:34:13 +0000

    add test exe, now parser is a library
    FossilOrigin-Name: 972c44d2584ccc67d4f825fa10e358bdd66cbf367ff5df9a64b2f68881744b82

---
+++ parser/tests/mdp_test.cpp
@@ -0,0 +1,56 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE modegen_parser
+
+#include <boost/test/unit_test.hpp>
+
+#include "grammar.hpp"
+
+using namespace std::literals;
+
+BOOST_AUTO_TEST_CASE(empty_module)
+{
+	std::vector<std::string_view> mods = {
+	    {"module mod v1:"sv}
+	  , {"#docs\nmodule mod v1:"sv}
+	  , {"#docs\n@v3 module mod v1:"sv}
+	};
+
+	for(auto mod:mods) BOOST_CHECK_EQUAL(modegen::parse(mod).size(), 1);
+}
+
+BOOST_AUTO_TEST_CASE(wrong_module)
+{
+	std::vector<std::string_view> mods = {
+	    {"module mod:"sv}
+	  , {"module\nmod v1:"sv}
+	  , {"module mod v1"sv}
+	  , {"#docs module mod v1:"sv}
+	  , {"#docs\n@v3 module mod v1"sv}
+	};
+
+	for(auto mod:mods) BOOST_CHECK_THROW(modegen::parse(mod), std::runtime_error);
+}
+
+BOOST_AUTO_TEST_CASE(module_params)
+{
+	auto mods = modegen::parse("#docs\nmodule mod v1:");
+	BOOST_REQUIRE_EQUAL(mods[0].meta_params.set.size(), 2);
+	BOOST_REQUIRE_EQUAL(mods[0].meta_params.set[0].index(), 1);
+	BOOST_REQUIRE_EQUAL(mods[0].meta_params.set[1].index(), 0);
+}
+
+BOOST_AUTO_TEST_SUITE(functions)
+BOOST_AUTO_TEST_CASE(simple)
+{
+	std::vector<std::string_view> mods = {
+	    {"module mod v1: type name();"sv}
+	  , {"#docs\nmodule mod v1: type other();"sv}
+	  , {"#docs\n@v3 module mod v1: type other();"sv}
+	  , {"module mod v1:\ntype name();"sv}
+	  , {"module mod v1:\ntype\nname();"sv}
+	  , {"module mod v1:\ntype\nname\n(\n)\n;"sv}
+	};
+
+	for(auto mod:mods) BOOST_CHECK_EQUAL(modegen::parse(mod).size(), 1);
+}
+BOOST_AUTO_TEST_SUITE_END()
