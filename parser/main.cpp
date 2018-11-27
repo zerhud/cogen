228dd9f2d0dce94413b61d6d12c8a79bf6136a3c
fleonis <fleonis@noemail.net>
2018-11-27 11:35:48 +0000

    create parser (can read file, write output, need to implement select parameter)
    FossilOrigin-Name: d00418f19c37989bcb19ba13594ece11feb944d2027998929a053b09cbefb68f

--- parser/main.cpp
+++ parser/main.cpp
@@ -2,53 +2,65 @@
 #include <iostream>
 #include <functional>
 
+#include <boost/program_options.hpp>
+
 #include "modegen.hpp"
 #include "grammar.hpp"
 
-void print_meta(const modegen::meta_parameters::parameter_set& params)
+#include "split_by_version.hpp"
+#include "to_json.h"
+
+namespace po = boost::program_options;
+
+std::string read_input(const std::string& file_name)
 {
-	std::cout << "\tmeta params:";
-	if(!params.set.empty())  std::cout << std::endl;
-	else {
-		std::cout << " no" << std::endl;
-		return;
+	if(file_name == "-") {
+		std::string pdata{std::istreambuf_iterator<char>(std::cin), std::istreambuf_iterator<char>()};
+		return pdata;
 	}
 
-	auto  printer = [](const auto& p){ std::cout << "\t\t" << p.name() << ": " << p.value() << std::endl; };
-	for(auto& param:params) std::visit(printer, param);
-	std::cout << "\t--" << std::endl;
-}
-
-void print_mod(const modegen::module& mod)
-{
-	std::cout << mod.name << ":" << std::endl << "\t" << mod.content.size() << std::endl;
-	print_meta(mod.meta_params);
-
-
-	for(auto& cnt:mod.content) {
-		auto* fnc = std::get_if<modegen::function>(&cnt);
-		if(fnc) {
-			std::cout << "\tfunc: " << fnc->return_type.name << " " << fnc->name << " ";
-			if(fnc->is_mutable) std::cout << *fnc->is_mutable;
-			else std::cout << "no_const" ;
-			std::cout << " => ";
-			for(auto& p:fnc->func_params) std::cout << p.param_type.name << " " << p.name << ", ";
-			std::cout << std::endl;
-			print_meta(fnc->meta_params);
-		}
-	}
-
-	std::cout << std::endl;
-}
-
-int main(int,char**)
-{
-	std::fstream file("pdata");
+	std::fstream file(file_name);
 	std::string pdata{std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>()};
+	return pdata;
+}
 
-	try{
+void write_output(const cppjson::value& mods, const std::string& file_name)
+{
+	if(file_name=="-") std::cout << mods << std::endl;
+	else {
+		std::fstream file(file_name);
+		file << mods;
+	}
+}
+
+int main(int argc,char** argv)
+{
+	po::options_description desc("Allowed options");
+	desc.add_options()
+	        ("help", "produce help message")
+	        ("input,i", po::value<std::string>()->default_value("-"), "input file")
+	        ("output,o", po::value<std::string>()->default_value("-"), "output file")
+	        ("split-by-versions,s", "split generated output by version (one module is one version)")
+	        ("select", po::value<std::string>()->default_value(""), "produce output only for selected path")
+	        ;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc,argv, desc), vm);
+	po::notify(vm);
+
+	if(vm.count("help")) {
+		std::cout << "use this program to prepare module data." << std::endl << desc << std::endl;
+		return 1;
+	}
+
+	std::string pdata = read_input(vm["input"].as<std::string>());
+
+	modegen::split_by_version splitter;
+
+	try {
 		auto result = modegen::parse(pdata);
-		for(auto& mod:result) print_mod(mod);
+		if(vm.count("split-by-version")) splitter(result);
+		write_output(modegen::converters::to_json(result), vm["output"].as<std::string>());
 	}
 	catch(const std::runtime_error& ex)
 	{
