b61c1e77a261900e7895206375d491b3fdd7bc85
fleonis <fleonis@noemail.net>
2018-12-06 05:50:17 +0000

    add realization cpp blank
    FossilOrigin-Name: 59313535813504af10ef391a9f3780191bc4e7612a2ad530092d346fb5306016

--- generators/cpp/realization.cpp
+++ generators/cpp/realization.cpp
@@ -5,10 +5,17 @@
 #include "to_json.h"
 #include "type_converter.h"
 
+using namespace boost::process;
+
 void modegen::generators::cpp::realization::generate(modegen::mod_selection query, std::vector<modegen::module> mods) const
 {
-	using namespace boost::process;
+	gen_hpp(query, mods);
+	std::cout << std::endl << std::endl;
+	gen_cpp(query, std::move(mods));
+}
 
+void modegen::generators::cpp::realization::gen_hpp(modegen::mod_selection query, std::vector<modegen::module> mods) const
+{
 	query.sel = module_content_selector::interface;
 
 	filter_by_selection(query, mods);
@@ -21,6 +28,29 @@ void modegen::generators::cpp::realization::generate(modegen::mod_selection quer
 	child a(
 	      "../generators/pythongen"
 	    , "-t", "../generators/cpp/realization.hpp.jinja"
+	    , "-o", (query.output ? query.output->generic_u8string() : "-")
+	    , std_out > stdout
+	    , std_in < pdata
+	    );
+	pdata << jsoned << std::endl;
+	pdata.pipe().close();
+	a.wait();
+}
+
+void modegen::generators::cpp::realization::gen_cpp(modegen::mod_selection query, std::vector<modegen::module> mods) const
+{
+	query.sel = module_content_selector::enumeration;
+
+	filter_by_selection(query, mods);
+	helpers::type_converter(query.sel, mods);
+	cppjson::value jsoned = modegen::converters::to_json(mods);
+
+	//TODO: add header file in includes
+
+	opstream pdata;
+	child a(
+	      "../generators/pythongen"
+	    , "-t", "../generators/cpp/realization.cpp.jinja"
 	    , "-o", (query.output ? query.output->generic_u8string() : "-")
 	    , std_out > stdout
 	    , std_in < pdata
--- generators/cpp/realization.h
+++ generators/cpp/realization.h
@@ -10,6 +10,9 @@ class realization : public modegen::generator
 {
 public:
 	void generate(mod_selection query, std::vector<modegen::module> mods) const override ;
+private:
+	void gen_hpp(mod_selection query, std::vector<modegen::module> mods) const ;
+	void gen_cpp(mod_selection query, std::vector<modegen::module> mods) const ;
 };
 
 } // namespace cpp
