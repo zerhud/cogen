f8783bef7ba459c1c17692bad79d492de07945fe
fleonis <fleonis@noemail.net>
2018-12-29 10:06:26 +0000

    add print_failed to test project (prints also failed test's names)
    FossilOrigin-Name: 8b2c21cd865bbcddc18701f57ab76922d70a426cb5718d0bf5528adb73b92f44

--- tests/srv-cpp/srv.cpp
+++ tests/srv-cpp/srv.cpp
@@ -3,6 +3,25 @@
 #include <iostream>
 #include "srv.hpp"
 
+std::vector<std::string_view> failed_tests;
+
+void print_failed()
+{
+	if(!failed_tests.empty()) {
+		std::cout << std::endl << "failed:" << std::endl;
+		for(auto& f:failed_tests) std::cout << f << std::endl;
+	}
+}
+
+void compare_str(std::string_view tn, const std::string& left, std::string_view right)
+{
+	if(left==right) std::cout << ".";
+	else {
+		std::cout << "!";
+		failed_tests.emplace_back(tn);
+	}
+}
+
 mod_v1_0::ugly_interface_ptr mod_v1_0::create_ugly_interface(std::string param)
 {
 	struct realization : mod_v1_0::ugly_interface {
@@ -17,13 +36,12 @@ mod_v1_0::ugly_interface_ptr mod_v1_0::create_ugly_interface(std::string param)
 int main(int, char**)
 {
 	mod_v1_0::ugly_interface_ptr uptr = mod_v1_0::create_ugly_interface("param");
-	if(uptr->get_param()=="param") std::cout << "." ;
-	else std::cout << "!" ;
+	compare_str("ugly_interface param", uptr->get_param(), "param");
 
 	std::stringstream e1_test;
-	e1_test << mod_v1_0::e1::one << mod_v1_0::e1::two ;
-	if(e1_test.str() == "onetwo") std::cout << "." ;
-	else std::cout << "!" ;
+	e1_test << mod_v1_0::e1::one << " " << mod_v1_0::e1::two ;
+	compare_str("mod::e1 putputs", e1_test.str(), "one two");
 
+	print_failed();
 	return 0;
 }
