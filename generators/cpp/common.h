ddff54e19d1287c4761df530da296432dba12763
fleonis <fleonis@noemail.net>
2019-01-30 21:15:29 +0000

    add files for common generation
    FossilOrigin-Name: c3a1183db6d39a2a75e396c203c7bd5b018d863389dd29223878a7996434122f

---
+++ generators/cpp/common.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "../generator.hpp"
+
+namespace modegen {
+namespace generation {
+namespace cpp {
+
+class common : public generator {
+public:
+	common(std::shared_ptr<generation_provider> prov) : gen_prov(std::move(prov)) {}
+
+	boost::property_tree::ptree& options() override {return opts;}
+	const boost::property_tree::ptree& options() const override {return opts;}
+
+	void path(const std::string& name, const std::string& value) override ;
+
+	void create_definitions(generation_request query, std::vector<modegen::module> mods) const override ;
+private:
+	std::shared_ptr<generation_provider> gen_prov;
+	boost::property_tree::ptree opts;
+};
+
+} // namespace cpp
+} // namespace generation
+} // namespace modegen
