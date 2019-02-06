5e6df520454e58bcfa8b54d3fa6bf9d0906f0b8e
fleonis <fleonis@noemail.net>
2019-02-06 23:45:41 +0000

    add tests for generate function
    FossilOrigin-Name: 50b00fee63c28d9aeaaf3c331076f92c86d8c5527e5c00be69da1242df81f010

--- next/tests/common_test.cpp
+++ next/tests/common_test.cpp
@@ -19,19 +19,27 @@ BOOST_AUTO_TEST_CASE(common_generation)
 {
 	// tests normal using
 
-	auto file_data_checker = [](const cppjson::value& data, const std::filesystem::path& tmpl, const std::optional<std::filesystem::path>& out) {
-			BOOST_CHECK( !out );
+	bool cout = true;
+	auto file_data_checker = [&cout](const cppjson::value& data, const std::filesystem::path& tmpl, const std::optional<std::filesystem::path>& out) {
+			if(cout) BOOST_CHECK( !out );
+			else {
+				BOOST_REQUIRE( out );
+				BOOST_CHECK_EQUAL( "some_dir" / (*out), "def.hpp" );
+			}
+
 			BOOST_CHECK_EQUAL( tmpl, "definitions.hpp.jinja" );
 			return true;
 		};
 
 	auto provider = std::make_shared<provider_mock>();
-	MOCK_EXPECT( provider->json_jinja ).once().with( file_data_checker ) ;
+	MOCK_EXPECT( provider->json_jinja ).exactly(2).with( file_data_checker ) ;
 
 	mg::generator gen(provider, u8"some/path");
 	auto& opts = gen.options();
 	opts.put("gen.def.output", "def.hpp") ;
 	opts.put("gen.def.input", "definitions.hpp.jinja") ;
 	gen.generate_stdout("def");
+	gen.generate("some_dir");
+	cout = false;
 }
 
