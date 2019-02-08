b8b500ff7faa4403ab669503783c678c0d5fb49f
fleonis <fleonis@noemail.net>
2019-02-08 23:02:52 +0000

    move checker test
    FossilOrigin-Name: 2fb351bac4b3d6b0c8f18717f4b4780afd969acf902ef3ed7933c22c62912e42

--- next/tests/interface/checker_test.cpp
+++ next/tests/interface/checker_test.cpp
@@ -3,63 +3,65 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include "check.h"
-#include "helpers.hpp"
+#include "errors.h"
+#include "parser/interface/check.hpp"
+#include "parser/interface/helpers.hpp"
 
 using namespace std::literals;
+namespace mi = modegen::parser::interface;
 
-void check_exception(modegen::checker& ch, modegen::parsed_file& fi, std::string_view path)
+void check_exception(mi::checker& ch, mi::parsed_file& fi, std::string_view path)
 {
-	auto ech = [&path](const modegen::error_info& i){
+	auto ech = [&path](const modegen::errors::error_info& i){
 		BOOST_CHECK_EQUAL( i.path, path );
 		return i.path==path;
 	};
 
 	fi.path = "test";
-	BOOST_CHECK_EXCEPTION(ch(fi), modegen::error_info, ech);
+	BOOST_CHECK_EXCEPTION(ch(fi), modegen::errors::error_info, ech);
 }
 
 BOOST_AUTO_TEST_SUITE(double_name)
 BOOST_AUTO_TEST_CASE(module)
 {
-	auto pf = modegen::parse("module mod v1.0: type name(); module mod2 v1.0: type name(); type name();"sv);
+	auto pf = mi::parse("module mod v1.0: type name(); module mod2 v1.0: type name(); type name();"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 2);
-	modegen::checker ch;
+	mi::checker ch;
 	check_exception(ch, pf, "mod2");
 	BOOST_CHECK_EQUAL(pf.mods[0].file_name, "test");
 	BOOST_CHECK_EQUAL(pf.mods[1].file_name, "test");
 }
 BOOST_AUTO_TEST_CASE(interface)
 {
-	auto pf = modegen::parse("module mod v1.0: interface i1{type name() const;} interface i2{type name() const; type name() const;}"sv);
+	auto pf = mi::parse("module mod v1.0: interface i1{type name() const;} interface i2{type name() const; type name() const;}"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 1);
-	modegen::checker ch;
+	mi::checker ch;
 	check_exception(ch, pf, "mod.i2");
 }
 BOOST_AUTO_TEST_CASE(double_ver)
 {
-	auto pf = modegen::parse("@v1.0 module mod v1.0: interface i1{type name() const;}"sv);
+	auto pf = mi::parse("@v1.0 module mod v1.0: interface i1{type name() const;}"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 1);
-	modegen::checker ch;
+	mi::checker ch;
 	check_exception(ch, pf, "mod");
 }
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_CASE(method_const_mark)
 {
-	auto pf = modegen::parse("module mod v1.0: interface i1{type name() const;} interface i2{type name();}"sv);
+	auto pf = mi::parse("module mod v1.0: interface i1{type name() const;} interface i2{type name();}"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 1);
-	modegen::checker ch;
+	mi::checker ch;
 	check_exception(ch, pf, "mod.i2.name");
 }
 
 BOOST_AUTO_TEST_CASE(mod_ver_is_min)
 {
-	auto pf = modegen::parse("module mod v10.0: @v9.0 type some(); module mod2 v10.0: interface i{@v9.0 type name()const;}"
+	auto pf = mi::parse("module mod v10.0: @v9.0 type some(); module mod2 v10.0: interface i{@v9.0 type name()const;}"
 	                           "module mod v9.0: @v10.1 interface i{@v10.0 type name()const;}"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 3);
-	modegen::checker ch;
-	modegen::parsed_file m1({pf.mods[0]}), m2({pf.mods[1]}), m3({pf.mods[2]});
+	mi::checker ch;
+	mi::parsed_file m1({pf.mods[0]}), m2({pf.mods[1]}), m3({pf.mods[2]});
 	check_exception(ch, m1, "mod.some");
 	check_exception(ch, m2, "mod2.i.name");
 	check_exception(ch, m3, "mod.i.name");
@@ -67,15 +69,15 @@ BOOST_AUTO_TEST_CASE(mod_ver_is_min)
 
 BOOST_AUTO_TEST_CASE(enum_gen_io)
 {
-	auto pf = modegen::parse("module mod v1.0: enum +auto_io some_enum {one two three=>'3'}"sv);
+	auto pf = mi::parse("module mod v1.0: enum +auto_io some_enum {one two three=>'3'}"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 1);
 	BOOST_REQUIRE_EQUAL(pf.mods[0].content.size(), 1);
 
-	modegen::enumeration& e = std::get<modegen::enumeration>(pf.mods[0].content[0]);
+	mi::enumeration& e = std::get<mi::enumeration>(pf.mods[0].content[0]);
 	BOOST_CHECK(e.gen_io);
 	BOOST_CHECK(e.elements[0].io.empty());
 
-	modegen::checker ch;
+	mi::checker ch;
 	BOOST_CHECK_NO_THROW(ch(pf));
 
 	BOOST_CHECK_EQUAL(e.elements.size(), 3);
@@ -87,10 +89,10 @@ BOOST_AUTO_TEST_CASE(enum_gen_io)
 BOOST_AUTO_TEST_SUITE(combination)
 BOOST_AUTO_TEST_CASE(diff_mods)
 {
-	auto pf = modegen::parse("module mod v1.0: type name(); module mod2 v1.0:"sv);
+	auto pf = mi::parse("module mod v1.0: type name(); module mod2 v1.0:"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 2);
 
-	modegen::checker ch;
+	mi::checker ch;
 	ch(pf);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 2);
 	BOOST_CHECK_EQUAL(pf.mods[0].name, "mod");
@@ -98,10 +100,10 @@ BOOST_AUTO_TEST_CASE(diff_mods)
 }
 BOOST_AUTO_TEST_CASE(diff_mods_ver)
 {
-	auto pf = modegen::parse("module mod v1.0: type name(); module mod v2.0:"sv);
+	auto pf = mi::parse("module mod v1.0: type name(); module mod v2.0:"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 2);
 
-	modegen::checker ch;
+	mi::checker ch;
 	ch(pf);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 2);
 	BOOST_CHECK_EQUAL(pf.mods[0].name, "mod");
@@ -109,10 +111,10 @@ BOOST_AUTO_TEST_CASE(diff_mods_ver)
 }
 BOOST_AUTO_TEST_CASE(combine)
 {
-	auto pf = modegen::parse("module mod v1.0: module mod v1.0: type name();"sv);
+	auto pf = mi::parse("module mod v1.0: module mod v1.0: type name();"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 2);
 
-	modegen::checker ch;
+	mi::checker ch;
 	auto mods = ch(pf).extract_result();
 	BOOST_REQUIRE_EQUAL(mods.size(), 1);
 	BOOST_CHECK_EQUAL(mods[0].name, "mod");
@@ -120,10 +122,10 @@ BOOST_AUTO_TEST_CASE(combine)
 }
 BOOST_AUTO_TEST_CASE(combine3)
 {
-	auto pf = modegen::parse("module mod v1.0: module mod v1.0: type name(); module mod v1.0: type name2();"sv);
+	auto pf = mi::parse("module mod v1.0: module mod v1.0: type name(); module mod v1.0: type name2();"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 3);
 
-	modegen::checker ch;
+	mi::checker ch;
 	auto mods = ch(pf).extract_result();
 	BOOST_REQUIRE_EQUAL(mods.size(), 1);
 	BOOST_CHECK_EQUAL(mods[0].name, "mod");
@@ -133,21 +135,21 @@ BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_AUTO_TEST_CASE(exports)
 {
-	auto pf = modegen::parse("module mod v1.0: interface i{} record r{} enum e{} void f();"sv);
+	auto pf = mi::parse("module mod v1.0: interface i{} record r{} enum e{} void f();"sv);
 	BOOST_REQUIRE_EQUAL(pf.mods.size(), 1);
-	modegen::checker ch;
+	mi::checker ch;
 	auto mods = ch(pf).extract_result();
 
 	BOOST_REQUIRE_EQUAL(mods[0].exports.size(), 4);
 	BOOST_CHECK_EQUAL(mods[0].exports[0].name, "i");
-	BOOST_CHECK_EQUAL(mods[0].exports[0].type, modegen::module_content_selector::interface);
+	BOOST_CHECK_EQUAL(mods[0].exports[0].type, mi::module_content_selector::interface);
 
 	BOOST_CHECK_EQUAL(mods[0].exports[1].name, "r");
-	BOOST_CHECK_EQUAL(mods[0].exports[1].type, modegen::module_content_selector::record);
+	BOOST_CHECK_EQUAL(mods[0].exports[1].type, mi::module_content_selector::record);
 
 	BOOST_CHECK_EQUAL(mods[0].exports[2].name, "e");
-	BOOST_CHECK_EQUAL(mods[0].exports[2].type, modegen::module_content_selector::enumeration);
+	BOOST_CHECK_EQUAL(mods[0].exports[2].type, mi::module_content_selector::enumeration);
 
 	BOOST_CHECK_EQUAL(mods[0].exports[3].name, "f");
-	BOOST_CHECK_EQUAL(mods[0].exports[3].type, modegen::module_content_selector::function);
+	BOOST_CHECK_EQUAL(mods[0].exports[3].type, mi::module_content_selector::function);
 }
