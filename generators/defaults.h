d2ccbccc08e91e4eb6ac5841a92ac5efd27255a7
fleonis <fleonis@noemail.net>
2019-02-15 16:05:15 +0000

    remove extra files, move integrated test into unsorted dir
    FossilOrigin-Name: 12067b5692b4af8c1bf1a190d20e4a01711278c441dc071bd00af6b146ac6e6b

--- generators/defaults.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "generator.hpp"
-
-namespace modegen::generation {
-
-void reg_default_generators(modegen::generator_maker* gm);
-
-} // namespace modegen::generators
--- parser/grammar.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string_view>
-#include "modegen.hpp"
-
-namespace modegen {
-modegen::parsed_file parse(std::string_view pdata);
-} // namespace modegen
