82ff4569f9c0b8547fbdae34d80d483c696d884b
fleonis <fleonis@noemail.net>
2019-02-24 22:10:44 +0000

    temporary disable warning: https://forum.rocketsoftware.com/t/mpd-setminalloc-warning-on-python-module-import/1315 in 201811 and https://stackoverflow.com/questions/49757245/fixing-warning-message-related-to-mpd-setminalloc here, but without answer
    FossilOrigin-Name: 022d8ea2e19d5b4414833ee302a5678089834c022a633bd03c0efb73146878dd

--- nix/default.nix
+++ nix/default.nix
@@ -7,14 +7,26 @@
 	, cppjson
 	, cmake
 	, ninja
-        , turtle
-        , clang
-        , llvm_7
-        , pybind11
+	, turtle
+	, clang
+	, llvm_7
+	, pybind11
 }:
 
 let
   clcov_deps = if enable_clcov then [clang llvm_7] else [];
+  python3_diswar = python3.overrideAttrs( old : {
+	  postPatch = old.postPatch + ''
+	  sed -i '55d' Modules/_decimal/libmpdec/context.c
+	  '';
+  });
+  pybind =
+	  (pybind11.override { python = python3_diswar; })
+	  .overrideAttrs( old : {
+		  doCheck = true;
+		  cmakeFlags = old.cmakeFlags ++ ["-DPYBIND11_TEST=OFF"];
+	  })
+  ;
 
 in
 stdenv.mkDerivation rec {
@@ -25,9 +37,9 @@ stdenv.mkDerivation rec {
 	nativeBuildInputs = [ cmake ninja ] ++ clcov_deps;
 	buildInputs = [
 		# for generation
-		python3 py_jinja
+		python3_diswar py_jinja
 		# for build excutable file
-		boost turtle pybind11
+		boost turtle pybind
 		cppjson
 	];
 }
