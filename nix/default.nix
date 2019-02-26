2b2f40a38c553d0dad9bba6696c5ca1b1e01eb66
fleonis <fleonis@noemail.net>
2019-02-26 15:42:31 +0000

    download lohman json
    FossilOrigin-Name: 46d4f36cd458802cb8b76105c3b8f14744a22de1843f406865f56979aff0c030

--- nix/default.nix
+++ nix/default.nix
@@ -34,6 +34,12 @@ stdenv.mkDerivation rec {
 	version = "0.0.0";
 	src = ./..;
 
+        nlohman_json_header = builtins.fetchurl {
+          #url = "https://github.com/nlohmann/json/raw/develop/single_include/nlohmann/json.hpp";
+          url = "https://github.com/nlohmann/json/raw/e326df211bd5b46c8ff9324cf093a932661da88a/single_include/nlohmann/json.hpp";
+          sha256 = "0643h79nmrzrlky1am1x46ihdha7b11fxv44q4b86sm0m0z2y31v";
+        };
+
 	nativeBuildInputs = [ cmake ninja ] ++ clcov_deps;
 	buildInputs = [
 		# for generation
