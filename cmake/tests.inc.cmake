6a6eb7157b783f5138c4cd6ff13c18c0bcbc31b8
fleonis <fleonis@noemail.net>
2019-02-27 08:31:21 +0000

    move tests cmake defs into sep file
    FossilOrigin-Name: 95979e44008d7defc26fdc02b1dd8e322dea025636f31b158f91c2365344932b

---
+++ cmake/tests.inc.cmake
@@ -0,0 +1,30 @@
+find_file(turtle turtle/mock.hpp)
+get_filename_component(turtle "${turtle}" DIRECTORY)
+get_filename_component(turtle "${turtle}" DIRECTORY)
+
+# unit tests...
+enable_testing()
+macro(add_unit_test tname path lib)
+	add_executable(${tname}_test "${path}${tname}_test.cpp")
+	target_link_libraries(${tname}_test PRIVATE ${lib} ${Boost_LIBRARIES} ${CMAKE_DL_LIBS})
+	target_include_directories(${tname}_test SYSTEM PRIVATE "${turtle}")
+	add_test(NAME ${tname} COMMAND ${tname}_test)
+endmacro()
+set(tests_gcommon
+	common
+	to_json
+	cpp cpp_type_cvt
+	name_conversion
+	filter
+	split_version
+	cmake
+	)
+set(tests_interface mdp checker )
+foreach(tname ${tests_interface})
+	add_unit_test(${tname} "tests/interface/" "modegen_interfaces")
+endforeach()
+foreach(tname ${tests_gcommon})
+	add_unit_test(${tname} "tests/generation/" "modegen_interfaces")
+endforeach()
+add_unit_test("dt_loader" "tests/data_tree/" "modegen_interfaces")
+
