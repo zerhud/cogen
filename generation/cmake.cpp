ce614f1f061f8cd45fbbc2af2388718cd2e6134d
fleonis <fleonis@noemail.net>
2019-06-05 21:47:07 +0000

    old uncommited changes.. it seems i was try to define cpppy module and add dependencies.
    FossilOrigin-Name: 8d33c77f1a8a50b4e594d640c68f65f53b45530d769b8e02cf77f4d8c32fc333

--- generation/cmake.cpp
+++ generation/cmake.cpp
@@ -27,16 +27,26 @@ nlohmann::json mg::cmake::jsoned_data(const output_info& outputs) const
 
 	auto libs = part.get_child_optional("libraries");
 	for(auto l:*libs) {
+		std::vector<std::string> deps;
 		std::vector<std::string> files;
 		for(auto& f:l.second) {
 			auto val = f.second.get_value<std::string>();
 			if(f.first=="file"sv) files.emplace_back(val);
 			//if(f.first=="part"sv) files.emplace_back(opts.get<std::string>(options::part_option::output, val));
 			if(f.first=="part"sv) files_from_part(files, outputs.require(val));
+			if(f.first=="dep"sv) deps.emplace_back(val);
 		}
 
 
 		data["libraries"][l.first]["files"] = files;
+		data["libraries"][l.first]["deps"] = deps;
+	}
+
+	auto deps = part.get_child_optional("deps");
+	if(deps) for(auto& d:*deps) {
+		nlohmann::json ddata;
+		ddata["pack"] = d.first;
+		data["deps"].push_back(ddata);
 	}
 
 	return data;
