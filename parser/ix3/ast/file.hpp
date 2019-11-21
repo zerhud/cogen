990e6c986d6af04016304253e3f060288061cc49
fleonis <fleonis@noemail.net>
2019-11-21 12:42:06 +0000

    add file parser
    FossilOrigin-Name: abbea8e9aa46287952a9efd8ec2f36d30d68b5f4ab9286c6996ac2b4484a081b

---
+++ parser/ix3/ast/file.hpp
@@ -0,0 +1,40 @@
+/*************************************************************************
+ * Copyright © 2019 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of modegen.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#pragma once
+
+#include "common.hpp"
+#include "entity.hpp"
+
+namespace ix3::ast {
+
+using module_content = x3::variant
+	< record
+	, function
+	, interface
+	, enumeration
+	>;
+
+struct include_st {
+	string_t path;
+};
+
+struct module {
+	string_t name;
+	std::vector<module_content> content;
+	meta::version version;
+	meta::set meta_params;
+};
+
+struct file_content {
+	std::vector<module> modules;
+	std::vector<include_st> includes;
+};
+
+} // namespace ix3::ast
+
---
+++ parser/ix3/grammar_def/file.hpp
@@ -0,0 +1,43 @@
+/*************************************************************************
+ * Copyright © 2019 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of modegen.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#pragma once
+
+#include <boost/spirit/home/x3/support/utility/annotate_on_success.hpp>
+
+#include "common.hpp"
+#include "grammar/common.hpp"
+#include "grammar/entity.hpp"
+#include "grammar/functions.hpp"
+#include "grammar/file.hpp"
+
+namespace ix3::text {
+
+auto const include_st_def   = lit("include") >> quoted2_string;
+auto const file_content_def = *(include_st) >> *(module);
+
+auto const module_def = meta_set
+	>> lit("module")
+	>> single_variable_name
+	>> meta_version
+	>> ':'
+	>> *(record | function | interface | enumeration)
+	;
+
+
+class  module_class : x3::annotate_on_success {} ;
+class  include_st_class : x3::annotate_on_success {} ;
+class  file_content_class : x3::annotate_on_success {} ;
+
+BOOST_SPIRIT_DEFINE( module )
+BOOST_SPIRIT_DEFINE( include_st )
+BOOST_SPIRIT_DEFINE( file_content )
+
+} // namespace ix3::text
+
+
