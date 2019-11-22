b2b99bb58fc1a2e4ed311f690a592c136de43f2d
fleonis <fleonis@noemail.net>
2019-11-22 16:16:19 +0000

    add operator < for version
    FossilOrigin-Name: 9bf9cbaaeb66f6585228d9e6ee1abc29de974b90e119f6a54e83471f6e958330

---
+++ parser/ix3/utils/meta.cpp
@@ -0,0 +1,15 @@
+/*************************************************************************
+ * Copyright © 2019 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of cppjinja.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#include "meta.hpp"
+
+bool ix3::ast::meta::operator < (const version& left, const version& right)
+{
+	return left.major_v < right.major_v && left.minor_v < right.minor_v;
+}
+
