219703d5cffbe0e8c588d600ce15370c4bf2edba
fleonis <fleonis@noemail.net>
2019-11-25 16:24:32 +0000

    forgot to add oapi to meta
    FossilOrigin-Name: 20ccff355b1f96d292aa883d8300a0524a108c30858c145db37c350f478c8377

--- parser/ix3/grammar_def/meta.hpp
+++ parser/ix3/grammar_def/meta.hpp
@@ -15,7 +15,7 @@
 
 namespace ix3::text {
 
-auto const meta_set_def = *(('@' >> meta_version) | meta_depricated | meta_documentation);
+auto const meta_set_def = *(('@' >> meta_version) | meta_depricated | meta_documentation | meta_oapi);
 auto const meta_oapi_def = lexeme[lit("@oapi") >> omit[+space] >> single_variable_name] >> lit("=") >> (quoted2_string | single_variable_name);
 auto const meta_version_def = 'v' >> x3::int_ >> '.' >> x3::int_;
 auto const meta_depricated_def = lit("@depricated") >> meta_version >> '(' >> quoted2_string >> ')';
