6f9ac7c446027723b53f6d45005c5886c0964a5a
fleonis <fleonis@noemail.net>
2019-12-03 19:24:50 +0000

    try to define config algos.. sync commit
    FossilOrigin-Name: cf72a119c2d41190f42da93e133dde8f111ebbf11891087e977615f48bba3570

---
+++ pg/configuration_algos/data.hpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+ * Copyright © 2019 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of modegen.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#pragma once
+
+#include "../configuration_algos.hpp"
+#include "parser/loader.hpp"
+
+namespace modegen::pg::conf_algos {
+
+class data_algos : public configuration_algos {
+public:
+	data_algos(const std::vector<modegen::parser::loader_ptr>& ldrs);
+	void set_filter(const options::part_view& pinfo) override ;
+	std::vector<std::string> map(const std::string& tmpl) const override ;
+	mapped_data map_to(mapped_data md) override ;
+	std::map<std::string,std::vector<std::string>> map_from(const std::string& tmpl) override ;
+};
+
+} // namespace modegen::pg::conf_algos
+
+
---
+++ pg/configuration_algos/ix3.cpp
@@ -0,0 +1,33 @@
+/*************************************************************************
+ * Copyright © 2019 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of modegen.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#include "ix3.hpp"
+
+using namespace modegen::pg::conf_algos;
+
+ix3_conf::ix3_conf(const std::vector<modegen:parser::loader_ptr>& ldrs)
+{
+}
+
+void ix3_conf::set_filter(const options::part_view& pinfo)
+{
+}
+
+std::vector<std::string> ix3_conf::map(const std::string& tmpl) const
+{
+}
+
+mapped_data ix3_conf::map_to(mapped_data md)
+{
+}
+
+std::map<std::string,std::vector<std::string>> ix3_conf::map_from(const std::string& tmpl)
+{
+}
+
+
