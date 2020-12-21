a6e0aaddaabf83139803c1d330f4ce9bc43bf786
zerhud <zerhud@noemail.net>
2020-12-21 12:23:36 +0000

    blank for mdg2, add executer. remove cppjinja from depends: cannot load libloader.so
    FossilOrigin-Name: 8e38a945a155a6fa5934345ac72f78f6456cdab9f823fb274c37474c809a3a81

--- mdg2/main.cpp
+++ mdg2/main.cpp
@@ -1,9 +1,20 @@
-#include "generation_part.hpp"
-#include "cppjinja/evtree/evtree.hpp"
-#include "cppjinja/parser/parse.hpp"
+/*************************************************************************
+ * Copyright © 2020 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of modegen.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
 
-int main(int,char**)
+#include "src/executer.hpp"
+#include "generation_part.hpp"
+//#include "cppjinja/evtree/evtree.hpp"
+//#include "cppjinja/parser/parse.hpp"
+
+int main(int argc,char** argv)
 {
-	cppjinja::evtree ev;
-	return 0;
+	mdg2::executer exe(argc, argv);
+	return exe();
+	//cppjinja::evtree ev;
+	//return 0;
 }
