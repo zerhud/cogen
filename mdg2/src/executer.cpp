f9dc8cffe4874550ff47a40ba8a18294cd9b1908
zerhud <zerhud@noemail.net>
2020-12-21 18:57:12 +0000

    add two modes blanks in executer
    FossilOrigin-Name: dfa79401607164c3fcb4616546e0abd82aa32178192910b5bc8e20e47c8bc35d

--- mdg2/src/executer.cpp
+++ mdg2/src/executer.cpp
@@ -13,17 +13,35 @@
 
 using namespace mdg2;
 namespace fs = std::filesystem;
+namespace po = boost::program_options;
+
+//boost::program_options::options_description executer::desc("mdg2 options");
 
 executer::executer(int argc, char** argv)
+	: desc("mdg2 options")
 {
 	assert(0 < argc);
 	set_pathes(argv[0]);
+	set_options();
+
+	auto opts = po::command_line_parser(argc, argv).options(desc).run();
+	po::store(opts, opt_vars);
 }
 
 void executer::set_pathes(fs::path cur_file)
 {
 	program_name = cur_file.stem();
 	self_path = fs::absolute(cur_file.parent_path()).lexically_normal();
+}
+
+void executer::set_options()
+{
+	desc.add_options()
+		("help,h", "produce this help message")
+		("outdir,o", po::value<std::string>(), "directory or file where to output")
+		("gmode,m", po::value<std::string>()->default_value("json"), "generation mode (\"json\" for generate json and \"dir\" to generate files)")
+		("input,i", po::value<std::vector<std::string>>(), "input (foramt like -Iinterface=some_file). use - for read from std input")
+		;
 }
 
 std::filesystem::path executer::etc_dir() const
@@ -42,6 +60,38 @@ std::filesystem::path executer::tmpl_dir() const
 
 int executer::operator()() const
 {
-	std::cout << tmpl_dir() << std::endl;
+	if(opt_vars.count("help"))
+		print_help();
+	else if(opt_vars["gmode"].as<std::string>()=="json")
+		json_mode();
+	else if(opt_vars["gmode"].as<std::string>()=="dir")
+		dir_mode();
+	else {
+		std::cerr
+			<< "wront generation mode "
+			<< opt_vars["gmode"].as<std::string>()
+			<< std::endl;
+		return 1;
+	}
+
 	return 0;
+}
+
+void executer::dir_mode() const
+{
+	std::cerr << "dir mode are not ready yet" << std::endl;
+}
+
+void executer::json_mode() const
+{
+	std::cerr << "json mode are not ready yet" << std::endl;
+}
+
+void executer::print_help() const
+{
+	std::cout
+		<< "this is a source code generator. use with options"
+		<< std::endl
+		<< desc
+		<< std::endl;
 }
--- mdg2/src/executer.hpp
+++ mdg2/src/executer.hpp
@@ -12,13 +12,20 @@
 namespace mdg2 {
 
 class executer {
+	boost::program_options::options_description desc;
+	boost::program_options::variables_map opt_vars;
+
 	std::filesystem::path self_path;
 	std::filesystem::path program_name;
 
 	void set_pathes(std::filesystem::path cur_file);
+	void set_options();
 
 	std::filesystem::path etc_dir() const ;
 	std::filesystem::path tmpl_dir() const ;
+	void print_help() const ;
+	void dir_mode() const ;
+	void json_mode() const ;
 public:
 	executer(int argc, char** argv);
 	int operator()() const ;
