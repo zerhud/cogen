0bdcee1c68b8eaf376d5d4a8e937d9fb00ddc89f
zerhud <zerhud@noemail.net>
2020-12-22 14:21:55 +0000

    add integral test
    FossilOrigin-Name: 0dd1d34fb05ab635d44075653779da8020c1f1d42314f250ace1adba35c4104c

---
+++ mdg2/tests/cpp_decl.py
@@ -0,0 +1,24 @@
+import subprocess as sp
+import json
+
+def check_example_json(result, tmpl, out):
+    assert 'file' in result[0]
+    assert 'out_file' in result[0]
+    assert 'data' in result[0]
+    assert result[0]['file'] == tmpl
+    assert result[0]['out_file'] == out
+    assert len(result[0]['data']) == 1
+    assert result[0]['data'][0]['name']=='ix3'
+    assert len(result[0]['data'][0]['mods'])==1
+
+def test_no_dir_yet():
+    r=sp.run(['./mdg2', '-m', 'dir', '-iix3=ix3_example'], stdout=sp.PIPE, stderr=sp.PIPE)
+    assert r.stderr == b'dir mode are not ready yet\n'
+
+def test_no_config():
+    r=sp.run(['./mdg2', '-m', 'json', '-iix3=ix3_example'], stdout=sp.PIPE, stderr=sp.PIPE)
+    assert r.stderr == b''
+    result = json.loads(r.stdout)
+    assert len(result) == 1
+    check_example_json(result, '', '')
+
