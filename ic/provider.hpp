ef938e333b9d200bfc68c1364fc734150efb4b59
zerhud <zerhud@noemail.net>
2021-01-03 18:37:07 +0000

    forgot to add provider.hpp ฅ(ﾐචﻌචﾐ)
    FossilOrigin-Name: ea9c570e265809d1a0e34fc3fb4997275347f90849cb92053be6e076278d327b

---
+++ ic/provider.hpp
@@ -0,0 +1,27 @@
+/*************************************************************************
+ * Copyright © 2020 Hudyaev Alexy <hudyaev.alexy@gmail.com>
+ * This file is part of modegen.
+ * Distributed under the GNU Affero General Public License.
+ * See accompanying file copying (at the root of this repository)
+ * or <http://www.gnu.org/licenses/> for details
+ *************************************************************************/
+
+#pragma once
+
+#include <filesystem>
+#include <boost/json.hpp>
+#include "declarations.hpp"
+
+namespace mdg::ic {
+
+class provider {
+public:
+	virtual ~provider() noexcept =default;
+
+	virtual void generate(
+	    std::filesystem::path tmpl_file,
+	    const boost::json::value& data,
+	    std::string_view out_file) const =0 ;
+};
+
+} // namespace mdg::ic
