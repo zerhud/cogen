34eb22df80d7f9bf4a00098e94d0ff448fdd8a6f
zerhud <zerhud@noemail.net>
2021-02-10 21:48:25 +0000

    add result tests in python, fix cpp_decl result compilation
    FossilOrigin-Name: 39baa3369c69f393bce2001a6b85cd3d581fafa1fb340fbab66474265fc1b11d

---
+++ mdg2/tests/results.py
@@ -0,0 +1,26 @@
+import subprocess as sp
+import shutil
+import os
+
+test_dir = 'test_dir'
+mdg_run = ['./mdgwr.py', '-o', test_dir, '-iix3=ix3_example', '-Ietc/mdg2/examples/incs']
+cpp_decl = ['-gcpp_decl']
+
+def setup_function():
+    if os.path.exists(test_dir):
+        shutil.rmtree(test_dir)
+
+def teardown_function():
+    if os.path.exists(test_dir):
+        shutil.rmtree(test_dir)
+
+def test_cpp_decl():
+    build_dir = test_dir + '/build'
+    r_mdg = sp.run(mdg_run + cpp_decl)
+    os.mkdir(build_dir)
+    r_cmake = sp.run(['cmake', '..'], cwd = build_dir)
+    r_make = sp.run(['cmake', '--build', '.'], cwd = build_dir)
+    assert r_make.returncode == 0
+    assert os.path.exists(build_dir + '/libinterface.so')
+    assert os.path.exists(build_dir + '/libinterface-static.a')
+
