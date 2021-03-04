ca16f3121025b506a644a38fe1eab1c04af230d6
zerhud <zerhud@noemail.net>
2021-03-04 23:13:30 +0000

    add find_package with python library
    FossilOrigin-Name: c6c54ee912f3b7b1f18f3e1b4f6c4e2aea8fe89662fbbc5169bd82aa3f763e46

--- mdg2/tests/results.py
+++ mdg2/tests/results.py
@@ -3,9 +3,9 @@ import shutil
 import os
 
 test_dir = 'test_dir'
-mdg_run = ['./mdgwr.py', '-o', test_dir, '-iix3=ix3_example', '-Ietc/mdg2/examples/incs']
+mdg_run = ['./mdgwr.py', '-iix3=ix3_example', '-Ietc/mdg2/examples/incs']
 cpp_decl = ['-gcpp_decl']
-cpp_py = ['-gcpp_decl']
+cpp_py = ['-gcpp_py']
 
 def setup_function():
     if os.path.exists(test_dir):
@@ -18,7 +18,7 @@ def teardown_function():
 
 def test_cpp_decl():
     build_dir = test_dir + '/build'
-    r_mdg = sp.run(mdg_run + cpp_decl)
+    r_mdg = sp.run(mdg_run + cpp_decl + ['-o', test_dir])
     os.mkdir(build_dir)
     r_cmake = sp.run(['cmake', '..'], cwd = build_dir)
     r_make = sp.run(['cmake', '--build', '.'], cwd = build_dir)
@@ -28,5 +28,9 @@ def test_cpp_decl():
 def test_cpp_py():
     build_dir = test_dir + '/build'
     py_dir = test_dir + '/gen'
-    r_mdg = sp.run(mdg_run + cpp_py)
+    r_mdg = sp.run(mdg_run + cpp_py + ['-o', test_dir])
+    os.mkdir(build_dir)
+    r_cmake = sp.run(['cmake', '../gen'], cwd = build_dir)
+    r_make = sp.run(['cmake', '--build', '.'], cwd = build_dir)
+    assert r_make.returncode == 0
 
