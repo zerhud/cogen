c638ec17259c43ed6cd1ed70606183fa9696c296
zerhud <zerhud@noemail.net>
2021-03-18 21:27:51 +0000

    cpppy test works. ticket: [24908ab30d].
    FossilOrigin-Name: dae74c1f26d06774f26ec48fe13c73d3ce4ce8ec0587fa8fc272bca104be2ed9

--- mdg2/tests/results.py
+++ mdg2/tests/results.py
@@ -41,7 +41,34 @@ def test_cpp_py():
     copy_resource_to_test('exmp_combine_module.cpp')
     copy_resource_to_test('exmp_external_module.cpp')
     copy_resource_to_test('exmp_cmake.cmake', 'CMakeLists.txt')
+    copy_resource_to_test('__init__.py', '__init__.py')
     r_cmake = sp.run(['cmake', '..'], cwd = build_dir)
-    r_make = sp.run(['cmake', '--build', '.'], cwd = build_dir)
+    r_make = sp.run(['cmake', '--build', '.', '-j4'], cwd = build_dir)
     assert r_make.returncode == 0
+
+    from test_dir.build import mod
+    from test_dir.build import combine_module
+    from test_dir.build import external_module
+
+    assert combine_module.combine([external_module.ExternalEnum.ex_two, external_module.ExternalEnum.ex_one]) == "ex_two ex_one "
+
+    ex_vd = external_module.VariusData()
+    external_module.fillData(ex_vd, "test msg")
+    assert ex_vd.message == "test msg"
+
+    assert external_module.genStr(external_module.ExternalEnum.ex_one) == "gen_one"
+
+    class EchoCallback(mod.EchoCallback):
+        def __init__(self):
+            super(EchoCallback, self).__init__()
+            self.count = 0
+        def onMessage(self, msg):
+            self.count += 1
+            assert msg == "test"
+    cb = EchoCallback()
+    srv = mod.CreateEchoService(10, cb)
+    msg = mod.EchoMesage()
+    msg.msg = "test"
+    srv.generate(msg)
+    assert cb.count == 1
 
