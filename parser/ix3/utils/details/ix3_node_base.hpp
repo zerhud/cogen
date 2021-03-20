b3d8fcd623143d56f7d7a72d78da939ffef39d9e
zerhud <zerhud@noemail.net>
2021-03-20 08:27:54 +0000

    rename to cogen in parsers
    FossilOrigin-Name: ad866de4451a692c2013b965a49cb426bdaf3ca1a046478fa43578b87e987b61

--- parser/ix3/utils/details/ix3_node_base.hpp
+++ parser/ix3/utils/details/ix3_node_base.hpp
@@ -1,6 +1,6 @@
 /*************************************************************************
  * Copyright © 2020 Hudyaev Alexy <hudyaev.alexy@gmail.com>
- * This file is part of modegen.
+ * This file is part of cogen.
  * Distributed under the GNU Affero General Public License.
  * See accompanying file copying (at the root of this repository)
  * or <http://www.gnu.org/licenses/> for details
