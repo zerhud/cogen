56f0fd0a59cbc62a4bab5b5158947e8e8859a21f
zerhud <zerhud@noemail.net>
2021-06-11 08:05:03 +0000

    up to gcc 11, start new project version. add dynamic language loading feature.
    FossilOrigin-Name: a0e100d590b1732908b2f1810ef2ff86b4b2f4ff0e19a8bd1779c7e16ddf8d8d

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { pkgs ? import<nixos-unstable>{} }:
 (import ../hup/default.nix {
   inherit pkgs;
-  stdenv = pkgs.gcc10Stdenv;
+  stdenv = pkgs.gcc11Stdenv;
   #stdenv = pkgs.llvmPackages_10.stdenv;
   enable_clcov=true;
 }).cogen
