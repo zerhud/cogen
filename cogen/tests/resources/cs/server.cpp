08b0503c8c8b8f7a6d2dc36d7d48148520229d3a
zerhud <zerhud@noemail.net>
2021-09-25 18:53:42 +0000

    add functions to server, add step tepmlate. bug: cannot get list of files for cmake can link it with program.. but is it realy need?
    FossilOrigin-Name: 6f09d2268074a67da32c164de5fc5c8bd3ab86c1b470d5761adeddc3029e8094

--- cogen/tests/resources/cs/server.cpp
+++ cogen/tests/resources/cs/server.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include "cs_1_0/module.hpp"
+#include "cs_1_0/server.hpp"
 
 int main(int,char**)
 {
-	std::cout << "The cs server was started!" << std::endl;
+	std::cout << "Ready to start cs server" << std::endl;
+	while(true) {
+		cs::step();
+	}
+	std::cout << "The cs server was finished!" << std::endl;
 	return 0;
 }
 
